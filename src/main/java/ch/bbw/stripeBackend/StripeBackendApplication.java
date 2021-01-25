09b989c1b72355c11b2c5fb7b5cd57aea3aac1bc
tietor <fabio.torti@tie.ch>
2021-01-25 08:20:46 +0100

    initial commit

---
+++ src/main/java/ch/bbw/stripeBackend/StripeBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.stripeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/stripeBackend/StripeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.stripeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
